2626e1d93be884e9320dbcee1027a0045c2d824d
Mihnea03 <stef1eds@gmail.com>
2022-12-22 14:21:51 +0200

    /

--- pandautil.py
+++ pandautil.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
-from re import Match
 
 user_df = pd.read_csv('customers.csv')
 
 def search_by_name(name):
     new_user_df = pd.DataFrame(columns = user_df.columns)
     for index, user in user_df.iterrows():
-        if type( re.match(name, user['name']) ) == Match[str]:
-            new_user_df.insert()
+        if re.match(name, user['name']) != None:
+            new_user_df.append({
+                'alpha': user['alpha'],
+                'name': user['name'],
+                'email': user['email'],
+                'date': user['date'],
+                'yn': user['yn']
+            })
+    print(new_user_df)
             
 
 def search_by_age():
